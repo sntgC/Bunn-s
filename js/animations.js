59b3a61696d23a8ef4d37edc5f842a7e5fa2f977
Santiago <sntgcortes23@gmail.com>
2016-10-29 20:05:20 -0500

    UI changes

--- js/animations.js
+++ js/animations.js
@@ -1,8 +1,11 @@
 window.onscroll = function() {navBarScroll()};
 function navBarScroll(){
 	if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
-        document.getElementById("header").style.background="#5bc0be"
+        //document.getElementById("header").style.background="#5bc0be";
+		//document.getElementById("header").style.background="#FF5900";
+		document.getElementById("header").style.background="#96e569";
     } else {
-        document.getElementById("header").style.background = "rgba(91, 192, 190,0)";
+        //document.getElementById("header").style.background = "rgba(91, 192, 190,0)";
+		document.getElementById("header").style.background = "rgba(255, 89, 0,0)";
     }
 }
