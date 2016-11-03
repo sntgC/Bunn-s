f66e25b0d6b5dc232a8883758aa521c795fcda94
Cameron <camd.99@gmail.com>
2016-11-03 18:00:06 -0500

    Google map is now larger and made small syntax corrections

--- js/animations.js
+++ js/animations.js
@@ -1,13 +1,13 @@
 var template;
 
-window.onscroll = function() {navBarScroll()};
+window.onscroll = function() {navBarScroll();};
 
-window.onload = function(){	
+window.onload = function(){
 	var source   = $("#menu-template").html();
 	console.log(source);
 	template = Handlebars.compile(source);
 	document.getElementById("menuMainContent").innerHTML=template(menuObj);
-}
+};
 
 function navBarScroll(){
 	if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
