827d6bab49618aeb7c7eb365e2d4d02799bdb118
Santiago <sntgcortes23@gmail.com>
2016-11-02 19:59:40 -0500

    Added templating

--- js/animations.js
+++ js/animations.js
@@ -8,3 +8,30 @@ function navBarScroll(){
 		document.getElementById("headerLogo").className = "navTitle noSelect logoClear";
     }
 }
+
+window.onload = function(){	
+	var source   = $("#menu-template").html();
+	console.log(source);
+	var template = Handlebars.compile(source);
+	var obj={row:[
+							{entry:[
+								{name:"a", image:"assets/city.jpg"},
+								{name:"b", image:"assets/city.jpg"},
+								{name:"c", image:"assets/city.jpg"}
+							]},
+							{entry:[
+								{name:"a", image:"assets/city.jpg"},
+								{name:"d", image:"assets/burger.jpg"},
+								{name:"c", image:"assets/city.jpg"}
+							]}
+						]};
+	console.log(template(obj));
+	document.getElementById("menuMainContent").innerHTML=template(obj);
+}
+
+function scroll(){
+	$('html, body').animate({
+								scrollTop: $("#menuMainContent").offset().top
+								}, 800, function(){
+	});
+}
