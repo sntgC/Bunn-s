4fec031cfd7f02581aa802647208a3f902cc8b74
Santiago <sntgcortes23@gmail.com>
2016-11-03 13:36:20 -0500

    Menu code for different items

--- js/animations.js
+++ js/animations.js
@@ -1,4 +1,14 @@
+var template;
+
 window.onscroll = function() {navBarScroll()};
+
+window.onload = function(){	
+	var source   = $("#menu-template").html();
+	console.log(source);
+	template = Handlebars.compile(source);
+	document.getElementById("menuMainContent").innerHTML=template(menuObj[0]);
+}
+
 function navBarScroll(){
 	if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
 		document.getElementById("header").style.background="#efd139";
@@ -9,29 +19,14 @@ function navBarScroll(){
     }
 }
 
-window.onload = function(){	
-	var source   = $("#menu-template").html();
-	console.log(source);
-	var template = Handlebars.compile(source);
-	var obj={row:[
-							{entry:[
-								{name:"a", image:"assets/city.jpg"},
-								{name:"b", image:"assets/city.jpg"},
-								{name:"c", image:"assets/city.jpg"}
-							]},
-							{entry:[
-								{name:"a", image:"assets/city.jpg"},
-								{name:"d", image:"assets/burger.jpg"},
-								{name:"c", image:"assets/city.jpg"}
-							]}
-						]};
-	console.log(template(obj));
-	document.getElementById("menuMainContent").innerHTML=template(obj);
-}
-
 function scroll(){
 	$('html, body').animate({
-								scrollTop: $("#menuMainContent").offset().top
+								scrollTop: $("#menuMainContent").offset().top-100
 								}, 800, function(){
 	});
+}
+
+function switchMenu(menuID){
+	document.getElementById("menuMainContent").innerHTML=template(menuObj[menuID]);
+	scroll();
 }
---
+++ js/objects.js
@@ -0,0 +1,69 @@
+var menuObjApp={row:[
+							{entry:[
+								{name:"Appetizers", image:"assets/city.jpg"},
+								{name:"b", image:"assets/city.jpg"},
+								{name:"c", image:"assets/city.jpg"}
+							]},
+							{entry:[
+								{name:"a", image:"assets/city.jpg"},
+								{name:"d", image:"assets/burger.jpg"},
+								{name:"c", image:"assets/city.jpg"}
+							]},
+							{entry:[
+								{name:"a", image:"assets/city.jpg"},
+								{name:"d", image:"assets/burger.jpg"},
+								{name:"c", image:"assets/city.jpg"}
+							]}
+						]};
+var menuObjEnt={row:[
+							{entry:[
+								{name:"Entree", image:"assets/city.jpg"},
+								{name:"b", image:"assets/city.jpg"},
+								{name:"c", image:"assets/city.jpg"}
+							]},
+							{entry:[
+								{name:"a", image:"assets/city.jpg"},
+								{name:"d", image:"assets/burger.jpg"},
+								{name:"c", image:"assets/city.jpg"}
+							]},
+							{entry:[
+								{name:"a", image:"assets/city.jpg"},
+								{name:"d", image:"assets/burger.jpg"},
+								{name:"c", image:"assets/city.jpg"}
+							]}
+						]};
+var menuObjDes={row:[
+							{entry:[
+								{name:"Dessert", image:"assets/city.jpg"},
+								{name:"b", image:"assets/city.jpg"},
+								{name:"c", image:"assets/city.jpg"}
+							]},
+							{entry:[
+								{name:"a", image:"assets/city.jpg"},
+								{name:"d", image:"assets/burger.jpg"},
+								{name:"c", image:"assets/city.jpg"}
+							]},
+							{entry:[
+								{name:"a", image:"assets/city.jpg"},
+								{name:"d", image:"assets/burger.jpg"},
+								{name:"c", image:"assets/city.jpg"}
+							]}
+						]};
+var menuObjDri={row:[
+							{entry:[
+								{name:"Drinks", image:"assets/city.jpg"},
+								{name:"b", image:"assets/city.jpg"},
+								{name:"c", image:"assets/city.jpg"}
+							]},
+							{entry:[
+								{name:"a", image:"assets/city.jpg"},
+								{name:"d", image:"assets/burger.jpg"},
+								{name:"c", image:"assets/city.jpg"}
+							]},
+							{entry:[
+								{name:"a", image:"assets/city.jpg"},
+								{name:"d", image:"assets/burger.jpg"},
+								{name:"c", image:"assets/city.jpg"}
+							]}
+						]};		
+var menuObj=[menuObjApp,menuObjEnt, menuObjDes, menuObjDri]						
