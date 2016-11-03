7bd4f80ffe1670b5719849fa7687cf60db859e17
Santiago <sntgcortes23@gmail.com>
2016-11-02 19:20:21 -0500

    Minor menu changes

--- js/animations.js
+++ js/animations.js
@@ -1,13 +1,10 @@
 window.onscroll = function() {navBarScroll()};
 function navBarScroll(){
 	if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
-        //document.getElementById("header").style.background="#5bc0be";
-		//document.getElementById("header").style.background="#FF5900";
-		//document.getElementById("header").style.background="#96e569";
-		//document.getElementById("header").style.background="#e1e512";
 		document.getElementById("header").style.background="#efd139";
+		document.getElementById("headerLogo").className = "navTitle noSelect logoSolid";
     } else {
-        //document.getElementById("header").style.background = "rgba(91, 192, 190,0)";
 		document.getElementById("header").style.background = "rgba(255, 89, 0,0)";
+		document.getElementById("headerLogo").className = "navTitle noSelect logoClear";
     }
 }
