b3919826b06d9a4bbe901f442c9b5dd48cf3d798
Santiago <sntgcortes23@gmail.com>
2017-02-06 20:58:27 -0600

    Added sample flip animation

--- js/style.js
+++ js/style.js
@@ -1,9 +1,22 @@
 var colors=["#B3CC57","#FFBE40","#6A259A","#F53753"];
+colors=["#00bb5c","#0e9dc9","#a465cb","#efd139"];
 window.onload=function(){
 	var fun=document.getElementsByClassName("fun");
 	shuffle(colors);
 	for(k=0;k<fun.length;k++)
 		fun[k].style.color=colors[k%colors.length];
+	
+	var arrows=document.getElementsByClassName("tokenArrow");
+	for(i=0;i<arrows.length;i++){
+		$(arrows[i]).on("click",getArrowFunction(i,arrows[i]));
+	}
+}
+
+function getArrowFunction(x, element){
+	return function(){
+		console.log(element.innerHTML);
+		$(element).parents(':eq(2)')[0].style.transform=(element.innerHTML+"").indexOf("left")>-1?"rotateY(0deg)":"rotateY(180deg)";
+		};
 }
 
 function shuffle(arr){
